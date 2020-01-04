a86af5f48a37866c2d954e556af93c0bd72cee81
Manish <manishkumar13899@gmail.com>
2020-01-04 22:49:26 +0530

    third commit

--- src/web3.js
+++ src/web3.js
@@ -1,7 +1,17 @@
 import Web3 from 'web3';
 
-
+let web3;
 // web3 from metamask
-const web3 = new Web3(window.web3.currentProvider);
-
+window.addEventListener('load',async ()=>{
+if(window.ethereum){
+    window.web3=new Web3(window.ethereum)
+    try{
+        await window.ethereum.enable();
+    }
+    catch(err){
+        console.log(err);
+    }
+}
+});
+web3=new Web3(window.web3.currentProvider);
 export default web3;
